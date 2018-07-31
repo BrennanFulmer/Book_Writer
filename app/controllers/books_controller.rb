021c58935e0a288e7654de8e23d5acb9ebf52d48
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-07-31 16:25:30 -0400

    Add routes and view for editing books
    Removed the normal find_by methods as backups for find_by_slug in all books controller routes, shouldn't be necessary and more likely to create false positives.
    Modified the new book form so the title param comes through as simply :title.
    The books by user view is now able to handle situations where the user doesn't exist.
    Added a section to books show view for displaying errors.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,11 +9,16 @@ class BooksController < ApplicationController
   end
 
   get "/books/by-user/:username" do
-    @user = User.find_by(username: params[:username]) ||
-    User.find_by_slug(params[:username])
-    @authenticated = logged_in?
-    @your_books = @user == current_user
-    erb :"/books/by_user"
+    @user = User.find_by_slug(params[:username])
+
+    if @user
+      @authenticated = logged_in?
+      @your_books = @user == current_user
+      erb :"/books/by_user"
+    else
+      flash[:message] = "Unable To Show Books By #{params[:username]}"
+      redirect "/books"
+    end
   end
 
   get "/books/new" do
@@ -27,7 +32,7 @@ class BooksController < ApplicationController
   end
 
   post "/books" do
-    @new_book = Book.new(title: params[:book][:title], user_id: session[:user_id])
+    @new_book = Book.new(title: params[:title], user_id: session[:user_id])
 
     if @new_book.save
       redirect "/books/#{@new_book.slug}"
@@ -38,8 +43,7 @@ class BooksController < ApplicationController
   end
 
   get "/books/:title" do
-    @book = Book.find_by(title: params[:title]) ||
-    Book.find_by_slug(params[:title])
+    @book = Book.find_by_slug(params[:title])
 
     if @book
       @user = current_user
@@ -49,6 +53,28 @@ class BooksController < ApplicationController
       flash[:message] = "No '#{params[:title]}' Book Found"
       redirect "/books"
     end
+  end
+
+  get "/books/:title/edit" do
+    @book = Book.find_by_slug(params[:title])
+    @user = current_user
+    @your_book = @user.id == @book.user_id if @user
+
+    if @book && @your_book
+      erb :"/books/edit"
+    elsif @book
+      flash[:message] = "You Don't Have Permission to Edit '#{params[:title]}'"
+      redirect "/books/<%= @book.slug %>"
+    else
+      flash[:message] = "No '#{params[:title]}' Book Found"
+      redirect "/books"
+    end
+  end
+
+  post "/books/:id" do
+    @book = Book.find_by(id: params[:id])
+    @book.update(title: params[:title])
+    redirect "books/#{@book.slug}"
   end
 
 end
